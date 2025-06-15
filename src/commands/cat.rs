b168729598cf61fc4281edcd2140350d4b237811
0Emperor <tounsadisaadsalah@gmail.com>
2025-06-15 11:16:53 +0100

    fixed wno args enable the user to write in the stdi

--- src/commands/cat.rs
+++ src/commands/cat.rs
@@ -1,10 +1,21 @@
 use std::fs;
+use std::io;
 pub fn cat(args:Vec<String>){
-    for file in args {
+    for file in &args {
             match fs::read_to_string(&file){
                 Ok(contents)=>println!("{}",contents),
                 Err(e) =>eprintln!("cat: {file}: {e}"),
             };
     }
+    if  args.len()==0{
+         loop {
+        let mut line = String::new();
+        let bytes = io::stdin().read_line(&mut line).unwrap();
+        if bytes == 0 {
+            break;
+        }
+        print!("{line}");
+    }
+    }
 }
-// when we enter only cat command we should enable the user to write in the stdin
+// when we enter only cat command we should enable the user to write in the stdi ------- done
